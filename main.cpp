82ebe3274a4354cc646724f92196de9eff8c23b8
Bryan Muscedere <bmuscede@uwo.ca>
2017-04-20 10:47:25 -0400

    Updated main.cpp to use the Clang header libraries.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
 #include "clang/Tooling/CommonOptionsParser.h"
@@ -12,9 +13,44 @@ static llvm::cl::OptionCategory RexCategory("Rex Options");
 
 const string DEFAULT_FILENAME = "a.out";
 
+static const string INCLUDE_DIR = "./include";
+static const string INCLUDE_DIR_LOC = "--extra-arg=-I" + INCLUDE_DIR;
+
+const char** prepareUpdatedArgs(int *argc, const char** argv){
+    //Checks if the folder exists.
+    boost::filesystem::path includePath(INCLUDE_DIR);
+    if (!boost::filesystem::is_directory(includePath)){
+        cerr << "Error: The include folders do not exist." << endl << "Clang cannot function without them." << endl;
+        _exit(1);
+    }
+
+    //Increments argc.
+    (*argc)++;
+
+    //Copies over the first argument.
+    char** updated = new char*[*argc];
+    updated[0] = new char[strlen(argv[0])];
+    strcpy(updated[0], argv[0]);
+
+    //Adds in the include argument.
+    updated[1] = new char[INCLUDE_DIR_LOC.size()];
+    strcpy(updated[1], INCLUDE_DIR_LOC.c_str());
+
+    //Next, loops through and copies.
+    for (int i = 1; i < (*argc - 1); i++){
+        updated[i + 1] = new char[strlen(argv[i])];
+        strcpy(updated[i + 1], argv[i]);
+    }
+
+    return (const char**) updated;
+}
+
 int main(int argc, const char **argv) {
+    //Processes the arguments.
+    const char** updatedArgv = prepareUpdatedArgs(&argc, argv);
+
     //Runs the processor.
-    CommonOptionsParser OptionsParser(argc, argv, RexCategory);
+    CommonOptionsParser OptionsParser(argc, updatedArgv, RexCategory);
     ClangTool Tool(OptionsParser.getCompilations(),
                    OptionsParser.getSourcePathList());
     int code = Tool.run(newFrontendActionFactory<ROSAction>().get());
